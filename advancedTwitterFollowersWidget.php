7907bd18dffe57a6a8bb4ac815117577d709fb32
alan.developer <alan.developer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-18 10:39:12 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/advanced-twitter-followers-widget/trunk@877086 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ advancedTwitterFollowersWidget.php
@@ -0,0 +1,2 @@
+<br />
+<b>Fatal error</b>:  Class 'WP_Widget' not found in <b>/home/sseo/public_html/1/advanced-twitter-followers-widget/advancedTwitterFollowersWidget.php</b> on line <b>13</b><br />
