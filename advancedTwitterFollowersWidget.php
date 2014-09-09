3d6f64827636a7b1d787d106afe9e9ca31b12306
alan.developer <alan.developer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-09 09:31:44 +0000

    some changes made
    git-svn-id: https://plugins.svn.wordpress.org/advanced-twitter-followers-widget/trunk@985151 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advancedTwitterFollowersWidget.php
+++ advancedTwitterFollowersWidget.php
@@ -349,8 +349,7 @@ $data .= "</div>
 			</div>
 		</div>
 	</div>";
-	if($author == "true"){
-	$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.liveherechicago.com' target='_blank' style='color: #808080;' title='more info...'>liveherechicago.com</a></div>";}
+	$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='https://plus.google.com/+Liveherechicago773' target='_blank' style='color: #808080;' title='LiveHereChicago.com'>Live Here Chicago</a></div>";
             echo $before_widget;
             echo $before_title . $title . $after_title;
 			if($consumerKey == "" && $consumerSecret == "" && $accessToken == "" && $accessTokenSecret == ""){
